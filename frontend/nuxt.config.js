58cd64242f1ce9f290e5a7557e8a14e70062e78f
kaku3 <kodakaku@gmail.com>
2021-03-13 18:16:20 +0900

    feat: pwa をやめる

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -44,8 +44,8 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
-    // https://go.nuxtjs.dev/pwa
-    '@nuxtjs/pwa',
+    // // https://go.nuxtjs.dev/pwa
+    // '@nuxtjs/pwa',
     // https://go.nuxtjs.dev/content
     '@nuxt/content',
     'nuxt-webfontloader'
@@ -59,12 +59,12 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {},
 
-  // PWA module configuration: https://go.nuxtjs.dev/pwa
-  pwa: {
-    manifest: {
-      lang: 'en',
-    },
-  },
+  // // PWA module configuration: https://go.nuxtjs.dev/pwa
+  // pwa: {
+  //   manifest: {
+  //     lang: 'en',
+  //   },
+  // },
 
   // Content module configuration: https://go.nuxtjs.dev/config-content
   content: {},
