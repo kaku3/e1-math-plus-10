83d3012c7cca2c2662b60fb16d081ccace1f9312
kaku3 <kodakaku@gmail.com>
2021-03-14 17:41:10 +0900

    feat: バランス調整

--- frontend/components/Game.vue
+++ frontend/components/Game.vue
@@ -137,7 +137,7 @@ import seAnswerNg from '~/assets/se/answer-ng.mp3'
 const ANSWER_TIME_DEFAULT = 10000  // endress : 1問あたりの回答時間
 const ANSWER_TIME_LEVELUP_COUNT = 5 // レベルアップ間隔
 const ANSWER_TIME_LEVELUP_TIME = 3000
-const ANSWER_TIME_MIN = 1500
+const ANSWER_TIME_MIN = 1200
 
 function time2(v: number) {
   return v.toFixed(2)
