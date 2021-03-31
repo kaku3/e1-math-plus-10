c7c947243867c88a769da2a552cdfdfe38d48177
kaku3 <kodakaku@gmail.com>
2021-04-01 00:54:23 +0900

    feat: ★

--- frontend/components/Calendar.vue
+++ frontend/components/Calendar.vue
@@ -1,17 +1,21 @@
 <template>
-  <v-card>
-    <v-calendar
-      :weekdays="[0, 1, 2, 3, 4, 5, 6]"
-      :type="month"
-    >
-      <template v-slot:day="{ date }">
-        <span v-for="(o, i) in starsOf(date)" :key="i" class="star-container"><v-icon class="shake">mdi-star</v-icon></span>
-      </template>
-    </v-calendar>
-  </v-card>
+  <v-fade-transition>
+    <v-card v-if="show">
+      <v-calendar
+        :weekdays="[0, 1, 2, 3, 4, 5, 6]"
+        :type="month"
+      >
+        <template v-slot:day="{ date }">
+          <span v-if="starsOf(date) >= 4" class="star-container"><v-icon class="shake">mdi-star</v-icon>{{ starsOf(date) }}</span>
+          <span v-else v-for="(o, i) in starsOf(date)" :key="i" class="star-container"><v-icon class="shake">mdi-star</v-icon></span>
+        </template>
+      </v-calendar>
+    </v-card>
+  </v-fade-transition>
 </template>
 <style lang="scss" scoped>
 .star-container {
+  font-family: 'Fredoka One';
   + .star-container {
     margin-left: -14px;
   }
@@ -45,8 +49,15 @@ import ScoreStore from '~/store/ScoreStore'
 import { ScoreEntity, GameMode } from '~/models/Score'
 
 export default Vue.extend({
+  data () {
+    return {
+      show: false
+    }
+  },
   mounted () {
-
+    this.$nextTick(() => {
+      this.show = true
+    })
   },
   methods: {
     starsOf(d:Date) {
@@ -58,7 +69,7 @@ export default Vue.extend({
         .filter((o:ScoreEntity) => o.createdAt >= start)
         .filter((o:ScoreEntity) => o.createdAt < end)
       if(scores.length >= 10) {
-        return 3
+        return Math.min(Math.floor(scores.length / 20) + 3, 6)
       } else if(scores.length >= 4) {
         return 2
       } else if(scores.length >= 1) {
