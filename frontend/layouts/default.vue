03720ed852513622f78fb62170cd5fff90a22f77
kaku3 <kodakaku@gmail.com>
2021-04-04 16:11:09 +0900

    feat: ナビゲーションメニュー修正

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -8,20 +8,23 @@
       app
     >
       <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
+        <v-list-item-group v-for="(oo, ii) in lists" :key="ii">
+          <section v-if="oo.section" :class="`${oo.cls}`">{{ oo.section }}</section>
+          <v-list-item
+            v-for="(item, i) in oo.items"
+            :key="i"
+            :to="item.to"
+            router
+            exact
+          >
+            <v-list-item-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title v-text="item.title" />
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
       </v-list>
     </v-navigation-drawer>
     <v-app-bar color="light-blue" :clipped-left="clipped" fixed app dark>
@@ -36,7 +39,19 @@
     </v-main>
   </v-app>
 </template>
+<style lang="scss" scoped>
+section {
+  color: white;
+  background-color: #00796B;
 
+  &.e1 {
+    background-color: #4CAF50;
+  }
+  &.e2 {
+    background-color: #2196F3;
+  }
+}
+</style>
 <script>
 export default {
   data() {
@@ -44,54 +59,79 @@ export default {
       clipped: false,
       drawer: false,
       fixed: false,
-      items: [
+      lists: [
         {
-          icon: 'mdi-home-circle',
-          title: 'ほーむ',
-          to: '/',
-        },
-        {
-          icon: 'mdi-playlist-edit',
-          title: 'たして１０',
-          to: {
-            name: 'game-plus-10',
-            params: {
-              mode: 'modeSprint',
-              count: 10
+          section: '',
+          cls: 'global',
+          items: [
+            {
+            icon: 'mdi-home-circle',
+            title: 'ほーむ',
+            to: '/',
             }
-          }
+          ]
         },
         {
-          icon: 'mdi-playlist-edit',
-          title: 'ヒトケタス',
-          to: '/game-plus-single',
+          section: '１ねんせい',
+          cls: 'e1',
+          items: [
+            {
+              icon: 'mdi-playlist-edit',
+              title: 'たして１０',
+              to: {
+                name: 'game-plus-10',
+                params: {
+                  mode: 'modeSprint',
+                  count: 10
+                }
+              }
+            },
+            {
+              icon: 'mdi-playlist-edit',
+              title: 'ヒトケタス',
+              to: '/game-plus-single',
+            },
+            {
+              icon: 'mdi-playlist-edit',
+              title: 'ひきざん２０',
+              to: {
+                name: 'game-minus-20',
+                params: {
+                  mode: 'minusSprint',
+                  count: 10
+                }
+              }
+            },
+          ]
         },
         {
-          icon: 'mdi-playlist-edit',
-          title: 'ひきざん２０',
-          to: {
-            name: 'game-minus-20',
-            params: {
-              mode: 'minusSprint',
-              count: 10
-            }
-          }
+          section: '２ねんせい',
+          cls: 'e2',
+          items: [
+            {
+              section: '',
+              icon: 'mdi-playlist-edit',
+              title: 'かけざん９９',
+              to: {
+                name: 'game-mul-99',
+                params: {
+                  mode: 'mul99Sprint',
+                  count: 10
+                }
+              }
+            },
+          ]
         },
         {
-          icon: 'mdi-playlist-edit',
-          title: 'かけざん９９',
-          to: {
-            name: 'game-mul-99',
-            params: {
-              mode: 'mul99Sprint',
-              count: 10
-            }
-          }
-        },
-        {
-          icon: 'mdi-account-box',
-          title: 'アカウント',
-          to: '/account',
+          section: 'せってい',
+          cls: 'setting',
+          items: [
+            {
+              icon: 'mdi-account-box',
+              title: 'アカウント',
+              to: '/account',
+            },
+          ]
         },
       ],
       miniVariant: false,
